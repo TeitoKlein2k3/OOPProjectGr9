31232d70b5c13c376834a266018f209ce115251f
quang2552003 <135411308+quang2552003@users.noreply.github.com>
2023-06-07 21:00:42 +0700

    Add files via upload

---
+++ OOP_project/src/form/Center.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package form;
+
+/**
+ *
+ * @author tth
+ */
+public class Center {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        formDangNhap fDN = new formDangNhap();
+        fDN.setLocationRelativeTo(null);
+        formDangKy fDK = new formDangKy();
+        fDK.setLocationRelativeTo(null);
+        formChinh fC = new formChinh();
+        fC.setLocationRelativeTo(null);
+    }
+}
