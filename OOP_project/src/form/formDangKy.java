029834c5ce002966339ed1aa674f4338d6997f7e
quang2552003 <nguyenquang2552003@gmail.com>
2023-06-08 10:09:01 +0700

    re-edit

--- OOP_project/src/form/formDangKy.java
+++ OOP_project/src/form/formDangKy.java
@@ -4,6 +4,8 @@
  */
 package form;
 
+import javax.swing.UIManager;
+
 /**
  *
  * @author tth
@@ -14,6 +16,7 @@ public class formDangKy extends javax.swing.JFrame {
      * Creates new form formDangKy
      */
     public formDangKy() {
+        setResizable(false);
         initComponents();
     }
 
@@ -189,20 +192,14 @@ public class formDangKy extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(formDangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(formDangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(formDangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(formDangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
         }
         //</editor-fold>
 
